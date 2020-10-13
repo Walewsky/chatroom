69da2989d1c595574bd95683df225a985bb43d92
Walewsky <walewsky@outlook.com>
2020-10-12 23:30:08 -0400

    Adding Chat Room Api

---
+++ ChatRoom/ChatRoom.Api/Migrations/20201012150807_InitialApiMigration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChatRoom.Api.Migrations
+{
+    public partial class InitialApiMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    User = table.Column<string>(type: "NVARCHAR(32)", nullable: false),
+                    Content = table.Column<string>(type: "NVARCHAR(512)", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "DATETIME2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_messages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "messages");
+        }
+    }
+}
---
+++ ChatRoom/ChatRoom.Api/Startup.cs
@@ -0,0 +1,120 @@
+using System.Reflection;
+using System.Threading.Tasks;
+using ChatRoom.Domain.Interfaces;
+using ChatRoom.Infrastructure;
+using ChatRoom.Infrastructure.Hubs;
+using ChatRoom.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ChatRoom.StockBot;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatRoom.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddDbContext<ChatRoomDbContext>(builder => builder.UseSqlServer(@"Server=localhost;Database=ChatRoom;Trusted_Connection=True;MultipleActiveResultSets=true", 
+                sqlOptions => sqlOptions.MigrationsAssembly(migrationsAssembly)));
+
+            services.AddTransient<IChatRepository, ChatRepository>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.Authority = "https://localhost:5001";
+                o.Audience = "ChatRoom";
+                o.RequireHttpsMetadata = false;
+                
+                o.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+
+                        // If the request is for our hub...
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/api/chat")))
+                        {
+                            // Read the token out of the query string
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ChatUser", policy => policy.RequireClaim("scope", "ChatRoom"));
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200");
+                    builder.AllowAnyHeader();
+                    builder.AllowAnyMethod();
+                    builder.AllowCredentials();
+                });
+            });
+
+            services.AddSignalR();
+
+            services.AddStockBot();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            InitializeDb(app);
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/api/chat/notify");
+            });
+        }
+
+        private static void InitializeDb(IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope();
+
+            serviceScope.ServiceProvider.GetRequiredService<ChatRoomDbContext>().Database.Migrate();
+        }
+    }
+}
